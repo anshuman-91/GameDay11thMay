b8340e9bea0b3a0b034d6f9b21c5a2249018acae
anshuman@simpledatalabs.com <anshuman@simpledatalabs.com>
2022-05-11 10:15:06 +0000

    init

--- code/scala/pipelines/Reformat/code/job/graph/Source_1.py
+++ code/scala/pipelines/Reformat/code/job/graph/Source_1.py
@@ -5,9 +5,17 @@ from job.config.ConfigStore import *
 from job.udfs.UDFs import *
 
 def Source_1(spark: SparkSession) -> DataFrame:
+
+    if Config.fabricName == "gamedayfabric":
+        return spark.read\
+            .option("header", True)\
+            .option("sep", ",")\
+            .csv("dbfs:/Prophecy/anshuman@simpledatalabs.com/timestamps_csv.csv")
+    else:
+        raise Exception("No valid dataset present to read fabric")
+
     if Config.fabricName == "anshuman2":
         return spark.read\
-            .schema(StructType([StructField("format", StringType(), True), StructField("example", TimestampType(), True)]))\
             .option("header", True)\
             .option("sep", ",")\
             .csv("dbfs:/Prophecy/anshuman@simpledatalabs.com/timestamps_csv.csv")
